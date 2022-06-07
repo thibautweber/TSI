1d571dcb83cab0b0dddfaf9e74d90f588e2c7c2b
Maxime Giroud <maxime.giroud@cpe.fr>
2022-06-07 12:06:51 +0200

    1

--- main.py
+++ main.py
@@ -38,12 +38,25 @@ def main():
     o              = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, Transformation3D())
     viewer.add_object(o)
 
-    vao     = Text.initalize_geometry()
-    texture = glutils.load_texture('fontB.jpg')
-    o       = Text('Bonjour les', np.array([-0.8, 0.3], np.float32), np.array([0.8, 0.8], np.float32), vao, 2, programGUI_id, texture)
+    m                    = Mesh.load_obj('cube.obj')
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1]))
+    tr                   = Transformation3D()
+    tr.translation.y     = -np.amin(m.vertices, axis=0)[1]
+    tr.translation.z     = -5
+    tr.rotation_center.z = 0.2
+    texture              = glutils.load_texture('cube.obj')
+    vitesse              = pyrr.Vector3()
+    o                    = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
     viewer.add_object(o)
-    o       = Text('3ETI', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
-    viewer.add_object(o)
+
+
+#    vao     = Text.initalize_geometry()
+#    texture = glutils.load_texture('fontB.jpg')
+#    o       = Text('Bonjour les', np.array([-0.8, 0.3], np.float32), np.array([0.8, 0.8], np.float32), vao, 2, programGUI_id, texture)
+#    viewer.add_object(o)
+#    o       = Text('3ETI', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
+#    viewer.add_object(o)
 
     viewer.run()
 
